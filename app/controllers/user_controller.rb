f37e5efe344a0cd35ae9f05172b70344f195df95
EarlGenest <charles.genestoux@gmail.com>
2018-10-30 14:58:49 +0100

    aller la

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,5 +5,16 @@ class UserController < ApplicationController
 
 	def create
 
+	puts "hello"
+	puts params
+	puts "fin"
+	puts "cest la data que je veux récup :#{params["username"]} "
+	user = User.new
+	user.username = (params["username"])
+	user.email = (params["email"])
+	user.bio = (params["bio"])
+	user.save
+
+
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  	root to: "user#new"
 
-	get '/new', to: 'user#new'
+	get "/new", to: "user#new"
+
+	post "/new", to: "user#create"
+
+	get "/new", to: "user#create"
 end
