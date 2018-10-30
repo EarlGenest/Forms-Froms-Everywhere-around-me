91b15faef80f70288f7a88feaefd44646617b202
EarlGenest <charles.genestoux@gmail.com>
2018-10-30 17:23:05 +0100

    quatre

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,18 +2,28 @@ class UserController < ApplicationController
 
 	def new
 
-		@user = User.all
+	@user = User.all 
+
+
+
+
 	end
 
 	def create
 
 
 
+
+	##################################### FROM_TAG
 	User.create username: params[:username]
 	User.create email: params[:email]
 	User.create bio: params[:bio]
-
 	redirect_to "/new"
+
+
+
+
+
 
 	end
 end
